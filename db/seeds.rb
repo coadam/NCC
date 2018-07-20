844d9659b24050ce9c2fc1ed20713a7073841ef3
coadam <adam756@gmail.com>
2018-07-20 21:59:46 +0200

    Add controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Quotation.destroy_all
+
+Quotation.create!([{
+  content: "Hell is that state where one has ceased to hope."
+},
+{
+  content: "To be or not to be."
+},
+{
+  content: "Test Quote"
+},
+{
+  content: "O, human love! thou spirit given, On Earth, of all we hope in Heaven!"
+}
+])
+
+p "Created #{Quotation.count} quotations"
